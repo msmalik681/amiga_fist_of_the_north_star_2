d464f0135611d7fee7b645069f967441242b9b00
Malik <31506138+msmalik681@users.noreply.github.com>
2024-06-15 16:05:11 +0100

    auto commit

--- project_folder/Template.tsx
+++ project_folder/Template.tsx
@@ -6,4 +6,9 @@
       <property name="Name" value="kenshiro" type="string" />
     </properties>
   </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="kenshiro_ducking" type="string" />
+    </properties>
+  </tile>
 </tileset>
